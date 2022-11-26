0d7d1db1364d5778db69ac3a0b981f0c344c6fb2
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-11-26 17:13:49 +0000

    feat: impl

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# Important
+# Installing
 
-- Does not work fully yet
+## Prebuilt binary
+- Download the appropriate `.so` file (the version number should match the version of nginx installed on your system) from the `build` directory
+- Copy the `.so` file to the nginx modules directory (typically `/etc/nginx/modules-enabled`)
+- Add the following line to the top of your `nginx.conf` file:
+```
+    load_module modules-enabled/interactions-verifier-1_18_0.so;
+```
+
+## Building from source
+- Clone this repository
+- Download nginx from the [nginx website](https://nginx.org/download/) & extract it
+- Switch to the new directory and run `./configure --add-dynamic-module=../module && make -j16 build modules`
+- The `ngx_discord_interactions_verifier.so` file will be located at the `nginx-x.x.x/objs/` directory
+- Follow the remaining steps from the "Prebuilt binary" section
+
+
+# Usage
+- After using the `load_module` directive, you can use the `verify_interactions` directive in your `server` or `location` block to verify interactions from Discord
+- Example:
+```
+http {
+  server {
+    listen 80;
+
+    location /interactions {
+      verify_interactions <Your public key here>;
+      proxy_pass http://localhost:xxxx;
+    }
+  }
+}
+```
+
+# Credits
+- ed25519 implementation by [orlp](https://github.com/orlp/ed25519)
--- module/ngx_discord_interactions_verifier.c
+++ module/ngx_discord_interactions_verifier.c
@@ -8,6 +8,12 @@ typedef struct {
   ngx_str_t public_key;
 } ngx_discord_interactions_verifier_loc_conf_t;
 
+typedef struct {
+  char *msg;
+  int read;
+  int total;
+} CTX;
+
 static void *ngx_discord_interactions_verifier_create_loc_conf(ngx_conf_t *cf);
 static char *ngx_discord_interactions_verifier_merge_loc_conf(ngx_conf_t *cf,
                                                               void *parent,
@@ -19,8 +25,12 @@ static char *ngx_handle_directives(ngx_conf_t *cf, ngx_command_t *cmd,
 static ngx_table_elt_t *search_headers_in(ngx_http_request_t *r, u_char *name,
                                           size_t len);
 
+int hex_to_bytes(const char *hex, unsigned char **bytes);
+
 static ngx_command_t ngx_discord_interactions_verifier_commands[] = {
-    {ngx_string("verify_interactions"), NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+    {ngx_string("verify_interactions"),
+     NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
+         NGX_CONF_TAKE1,
      ngx_handle_directives, NGX_HTTP_LOC_CONF_OFFSET, 0, NULL},
     ngx_null_command};
 
@@ -48,17 +58,58 @@ ngx_module_t ngx_discord_interactions_verifier = {
     NULL,
     NGX_MODULE_V1_PADDING};
 
-static ngx_http_output_body_filter_pt ngx_http_next_body_filter;
+static ngx_http_request_body_filter_pt ngx_http_next_request_body_filter;
 
 static ngx_int_t verify(ngx_http_request_t *r, ngx_chain_t *in) {
   ngx_discord_interactions_verifier_loc_conf_t *conf;
   ngx_chain_t *cl;
   u_char *p;
+  CTX *ctx;
 
   conf = ngx_http_get_module_loc_conf(r, ngx_discord_interactions_verifier);
+  ctx = ngx_http_get_module_ctx(r, ngx_discord_interactions_verifier);
 
   if (!conf->enable_verification) {
-    return ngx_http_next_body_filter(r, in);
+    return ngx_http_next_request_body_filter(r, in);
+  }
+
+  if (ctx == NULL) {
+    static const char *sign = "X-Signature-Ed25519";
+    static const char *ts = "X-Signature-Timestamp";
+
+    ngx_table_elt_t *signature_h =
+        search_headers_in(r, (unsigned char *)sign, strlen(sign));
+    ngx_table_elt_t *timestamp_h =
+        search_headers_in(r, (unsigned char *)ts, strlen(ts));
+
+    if (signature_h == NULL || timestamp_h == NULL ||
+        signature_h->value.len == 0 || timestamp_h->value.len == 0) {
+      return NGX_HTTP_FORBIDDEN;
+    }
+
+    ctx = ngx_pcalloc(r->pool, sizeof(CTX));
+    if (ctx == NULL) {
+      return NGX_ERROR;
+    }
+    ngx_http_set_ctx(r, ctx, ngx_discord_interactions_verifier);
+
+    ctx->total = r->headers_in.content_length_n;
+    ctx->read = 0;
+    ctx->msg = ngx_pcalloc(r->pool, ctx->total);
+  }
+
+  if (ctx->read < ctx->total) {
+    for (cl = in; cl; cl = cl->next) {
+      p = cl->buf->pos;
+      for (p = cl->buf->pos; p < cl->buf->last; p++) {
+        strncat(ctx->msg, (const char *)p, 1);
+        ctx->read++;
+      }
+    }
+  }
+
+  if (ctx->read < ctx->total) {
+    return ngx_http_next_request_body_filter(r, in);
   }
 
   static const char *sign = "X-Signature-Ed25519";
@@ -71,7 +122,6 @@ static ngx_int_t verify(ngx_http_request_t *r, ngx_chain_t *in) {
 
   if (signature_h == NULL || timestamp_h == NULL ||
       signature_h->value.len == 0 || timestamp_h->value.len == 0) {
-    r->keepalive = 0;
     return NGX_HTTP_FORBIDDEN;
   }
 
@@ -79,34 +129,41 @@ static ngx_int_t verify(ngx_http_request_t *r, ngx_chain_t *in) {
   char *timestamp = (char *)timestamp_h->value.data;
   char *pk = (char *)conf->public_key.data;
 
-  char *msg =
-      (char *)malloc(1 + strlen(signature) + r->headers_in.content_length_n);
+  char *msg = (char *)malloc(1 + strlen(timestamp) + strlen(ctx->msg));
   strcpy(msg, timestamp);
+  strcat(msg, ctx->msg);
 
-  for (cl = in; cl; cl = cl->next) {
-    for (p = cl->buf->pos; p < cl->buf->last; p++) {
-      strncat(msg, (char *)p, 1);
-    }
+  unsigned char *signature_bytes = (unsigned char *)malloc(1);
+  unsigned char *pk_bytes = (unsigned char *)malloc(1);
+
+  if (hex_to_bytes(signature, &signature_bytes) < 0) {
+    // free (ctx);
+    free(msg);
+    free(signature_bytes);
+    free(pk_bytes);
+    r->keepalive = 0;
+    return NGX_HTTP_FORBIDDEN;
   }
 
-  unsigned char signature_bytes[64];
-  unsigned char public_key_bytes[32];
-
-  for (size_t count = 0;
-       count < sizeof signature_bytes / sizeof *signature_bytes; count++) {
-    sscanf(signature, "%2hhx", &signature_bytes[count]);
-    signature += 2;
+  if (hex_to_bytes(pk, &pk_bytes) < 0) {
+    // free (ctx);
+    free(msg);
+    free(signature_bytes);
+    free(pk_bytes);
+    r->keepalive = 0;
+    return NGX_HTTP_FORBIDDEN;
   }
 
-  for (size_t count = 0;
-       count < sizeof public_key_bytes / sizeof *public_key_bytes; count++) {
-    sscanf(pk, "%2hhx", &public_key_bytes[count]);
-    pk += 2;
-  }
+  int verified = ed25519_verify(signature_bytes, (const unsigned char *)(msg),
+                                strlen(msg), pk_bytes);
 
-  if (ed25519_verify(signature_bytes, (const unsigned char *)msg, strlen(msg),
-                     public_key_bytes)) {
-    return ngx_http_next_body_filter(r, in);
+  // free (ctx);
+  free(msg);
+  free(signature_bytes);
+  free(pk_bytes);
+
+  if (verified) {
+    return ngx_http_next_request_body_filter(r, in);
   } else {
     r->keepalive = 0;
     return NGX_HTTP_FORBIDDEN;
@@ -139,8 +196,9 @@ static char *ngx_discord_interactions_verifier_merge_loc_conf(ngx_conf_t *cf,
 }
 
 static ngx_int_t ngx_discord_interactions_verifier_init(ngx_conf_t *cf) {
-  ngx_http_next_body_filter = ngx_http_top_body_filter;
-  ngx_http_top_body_filter = verify;
+
+  ngx_http_next_request_body_filter = ngx_http_top_request_body_filter;
+  ngx_http_top_request_body_filter = verify;
 
   return NGX_OK;
 }
@@ -217,4 +275,18 @@ static ngx_table_elt_t *search_headers_in(ngx_http_request_t *r, u_char *name,
   No headers was found
   */
   return NULL;
+}
+
+int hex_to_bytes(const char *hex, unsigned char **bytes) {
+  size_t len = strlen(hex);
+  if (len % 2 != 0) {
+    return -1;
+  }
+  size_t final_len = len / 2;
+
+  *bytes = (unsigned char *)malloc((final_len + 1) * sizeof(*bytes));
+  for (size_t i = 0, j = 0; j < final_len; i += 2, j++)
+    (*bytes)[j] = (hex[i] % 32 + 9) % 25 * 16 + (hex[i + 1] % 32 + 9) % 25;
+  (*bytes)[final_len] = '\0';
+  return 1;
 }
